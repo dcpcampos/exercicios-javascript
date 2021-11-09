7ec2481aef3b0589f9a680d29129a117a2271cf1
Diego <mail.daierol@gmail.com>
2021-11-09 09:55:36 -0300

    Exercicio Js 1

---
+++ js1/scripts.js
@@ -0,0 +1,17 @@
+var nomeVelha = prompt("Informe o nome da pessoa mais velha");
+var idadeVelha = prompt("Informe a idade e da pessoa mais velha");
+
+var nomeNova = prompt("Informe o nome da pessoa mais nova");
+var idadeNova = prompt("Informe a idade e da pessoa mais nova");
+
+var diff = idadeVelha - idadeNova;
+
+alert(
+    "Pessoa mais velha: " + nomeVelha + "\nIdade: " + idadeVelha + 
+    "\n\nPessoa mais nova: " + nomeNova + "\nIdade: " + idadeNova + 
+    "\n\nDiferença de idade: " + diff);
+
+
+
+
+
