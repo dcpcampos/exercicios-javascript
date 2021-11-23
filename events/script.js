3932a810f2e7c61782510df8dafcb8ed8ffe25c2
Diego <92345603+ykeido@users.noreply.github.com>
2021-11-23 09:07:30 -0300

    eventos

---
+++ events/script.js
@@ -0,0 +1,20 @@
+function clickMe(){
+    console.log("Fui clicado")
+}
+
+function FocusMe(){
+    console.log("Fui Focado")
+}
+
+function BlursMe(){
+    console.log("Fui Desfocado")
+}
+
+function ChangeMe(){
+    console.log("Fui Alterado")
+}
+
+
+function OverMe(){
+    console.log("Mouse aqui")
+}
